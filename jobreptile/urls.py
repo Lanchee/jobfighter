c38dad25872c277b945c5d876ff498da8fe62673
Lanchee <lanchee@yeah.net>
2017-07-08 11:53:20 +0800

    fix some problem
    1.修复list网页正则表达式匹配失败问题
    2.添加Sqlite数据库
    3.增加对url参数传递的部分测试代码和注释

--- jobreptile/urls.py
+++ jobreptile/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^home/', views.home, name='home_name'),
     url(r'^index/', views.index, name='index_name'),
-    url(r'^list/[a-zA-Z]+/$', views.list, name='list_name'),
+    url(r'^list/(.+)/', views.list, name='list_name'),
+    url(r'^skill/$', views.skill, name='skill_name'),
 ]
--- jobreptile/views.py
+++ jobreptile/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render_to_response,render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
 
 def index(request):
     # return render_to_response("home.html")
     # return HttpResponse("line")
     return render(request, "index.html")
 
+
 def home(request):
     return render(request, 'home.html')
 
-def list(request):
-    return render(request, 'list.html')
+
+def list(request, skill):
+    return HttpResponse("the param is:" + skill)
+    # return render(request, 'list.html')
+
+def skill(request):
+    skill = request.GET.get('skills')
+    return HttpResponse("the param is:" + skill)
 
 # Create your views here.
